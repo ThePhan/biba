c828951b59df084ef3baf2828521d57d3c1a37c0
ThePhan <phanxuanthe94@gmail.com>
2016-08-10 00:13:14 +0700

    add toastr

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,3 +1,13 @@
-routerApp.controller('homeController',  function($scope, $http, $state){
+routerApp.controller('homeController',  function($scope, $http, $state, toastr){
 
+  $scope.signUp = function(){
+    var a = 9;
+    if (a = 8) {
+
+      toastr.error('Your list is empty.', 'Warning');
+    }else {
+      toastr.error('Your list is empty.', 'Warning');
+
+    }
+  };
 });
